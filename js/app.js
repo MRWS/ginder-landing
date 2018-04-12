b610e3631685be0e4a1dde281f8ec39b798fae99
Samy Yahia <samy.yahia@hetic.net>
2018-04-12 17:07:38 +0200

    Update Css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,77 @@
 var app = {
     init: function () {
+      if(window.addEventListener){
+          window.addEventListener("scroll", function() {
+              app.animate();
+          }, false);
+      }else{
+          window.attachEvent("scroll", function(){
+              app.animate();
+          });
+      }
+
       app.checkIE();
+      app.chartBuilder();
+      app.counter();
+      app.animate();
+    },
+    chartBuilder: function() {
+      var charts = document.querySelectorAll('.canvas_chart');
+      charts.forEach(function(chart, k) {
+        var value = parseInt(chart.parentNode.querySelector('.chart_stat .value').getAttribute('data-value'));
+        chart = chart.getContext('2d');
+        var config = {
+          type: 'doughnut',
+          data: {
+            datasets: [{
+              data: [
+                value,
+                100 - value,
+              ],
+              backgroundColor: [
+                "#05476C",
+                "white",
+              ],
+              label: 'Dataset 1'
+            }],
+          },
+          options: {
+            responsive: false,
+            legend: {
+              position: 'top',
+            },
+            animation: {
+              animateScale: true,
+              animateRotate: true
+            }
+          }
+        };
+        window.myDoughnut = new Chart(chart, config);
+      });
+
+    },
+    counter: function() {
+      var stats = document.querySelectorAll('.chart_stat');
+      for (var i = 0; i < stats.length; i++) {
+        var stat = stats[i];
+
+        app.test(stat);
+      }
+    },
+    test: function(elem) {
+      var value       = elem.querySelector('span.value'),
+          stat        = parseInt(value.innerText),
+          startNumber = 0;
+
+      value.innerText = startNumber;
+      var lol = setInterval(function () {
+        if (startNumber < stat) {
+          startNumber++;
+          value.innerText = startNumber;
+        } else {
+          clearInterval(lol);
+        }
+      }, 1500 / stat);
     },
     checkIE : function(){
       var nav =   window.navigator.userAgent,
@@ -12,6 +83,21 @@ var app = {
           document.body.classList.add('ie');
           document.body.classList.add('ie'+version);
       }
+    },
+    animate: function(){
+      var _wa = document.querySelectorAll('.wa');
+      for (var i = 0; i < _wa.length; i++) {
+        var _this    = _wa[i],
+            _style   = window.getComputedStyle(_this),
+            _wh      = screen.height - parseInt(_style.marginTop) - parseInt(_style.marginBottom),
+            _offsetT = _this.offsetTop + parseInt(_style.marginTop) + _wh * .05,
+            _scroll  = window.scrollY,
+            _screen  = _scroll + _wh;
+
+      if (_screen >= _offsetT) {
+        _this.classList.add("animate");
+      }
+    }
   },
 }
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css'
 import '../styles/index.scss'
 import 'particles.js'
+import 'Chart.js'
 import app from './app.js'
 
 particlesJS.load('particles-js', 'particles.json', function () {
