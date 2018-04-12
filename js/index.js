10372696b6595c020bcd84fe5a1b5d2c823ca816
Marwan SENTENAC <m.sentenac@gmail.com>
2018-04-12 10:42:24 +0200

    init particles

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 import 'normalize.css'
 import '../styles/index.scss'
+import 'particles.js'
+
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('particles.js config loaded')
+})
